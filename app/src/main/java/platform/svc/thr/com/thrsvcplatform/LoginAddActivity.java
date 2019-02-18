f8e90a4a76c30b119dcdd30d32910b8da7a4b7dd
IntaekOh <itoh@shi-ict.com>
2019-02-18 09:26:56 +0900

    Google Login

--- app/src/main/java/platform/svc/thr/com/thrsvcplatform/LoginAddActivity.java
+++ app/src/main/java/platform/svc/thr/com/thrsvcplatform/LoginAddActivity.java
@@ -1,11 +1,9 @@
 package platform.svc.thr.com.thrsvcplatform;
 
 import android.content.Intent;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
